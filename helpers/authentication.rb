1b08c1392357a2b2b518c302a8f8c7ad33b214b1
Russell Miller <russell@adamm.net>
2014-12-28 22:14:27 -0500

    adding routes:

---
+++ helpers/authentication.rb
@@ -0,0 +1,24 @@
+helpers do
+	def current_user
+		return nil unless session.key?(:user_id)
+		@current_user ||= User.get(session[:user_id])
+	end
+
+	def user_signed_in?
+		!current_user.nil?
+	end
+
+	def sign_in!(user)
+		session[:user_id] = user.id
+		@current_user = user
+	end
+
+	def sign_out!
+		@current_user = nil
+		session.delete(:user_id)
+	end
+
+end
+
+set(:sessions, true)
+set(:session_secret, ENV["SESSION_SECRET"])
